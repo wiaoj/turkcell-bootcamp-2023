01d732e33523c0873230819b423bd5cb1d983bf4
wiaoj <bertandeniz7@gmail.com>
2023-03-20 21:15:15 +0300

    lesson 7, 20.03.2023

---
+++ C#/OOP/NullConcept/GenericTypes/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+
+ArrayList words = new() {
+    //words.Add("String");
+    1
+};
+//words.Add(false);
+
+//Int32 number = (Int32)words[0];
+String? number = words[0] as String;
+
+Console.WriteLine(number); // Boxing ve unboxing sadece mecbur olunca kullanılmalı
+
+GenericPoint<Decimal> genericPoint = new();
+//GenericPoint<Random> genericPoint2 = new(); Type belirlendiği için kısıtlama getirilebiliyor
+String word = "Selam";
+//Console.WriteLine(GetTypeOfGeneric(word, word));
+//Console.WriteLine(GetTypeOfGeneric<String, String>(word, word));
+
+String a = "Merhaba";
+String b = "Selam";
+
+Console.WriteLine($"A: {a}, B: {b}");
+
+Swap<String>(ref a, ref b);
+
+Console.WriteLine($"A: {a}, B: {b}");
+(a, b) = (b, a);
+Console.WriteLine($"A: {a}, B: {b}");
+
+List<String> items = new();
+
+items.Add(word);
+// Generic Interface'de varyant kavramının araştırılması.
+
+
+//Type GetTypeOfGeneric<Type, W>(Type type) where Type : class, new() {
+//    Type t = new();
+//    return t;
+//}
+
+String GetTypeOfGeneric<Type, W>(Type type, W type2) where Type : class, new()
+    where W : class, new() {
+    Type t = new();
+    //if(type is String) {
+    //    return "Bu bir stringdir.";
+    //}
+    return type.GetType().Name;
+}
+
+
+void Swap<Type>(ref Type t1, ref Type t2) {
+    Type temp = t1;
+
+    t1 = t2;
+    t2 = temp;
+}
+
+public class Point {
+    public Int32 X { get; set; }
+    public Int32 Y { get; set; }
+
+    public void Reset() {
+        this.X = 0;
+        this.Y = 0;
+    }
+}
+
+public class DoublePoint {
+    public Double X { get; set; }
+    public Double Y { get; set; }
+
+    public void Reset() {
+        this.X = 0.0D;
+        this.Y = 0.0D;
+    }
+}
+
+public class ObjectPoint {
+    public Object X { get; set; }
+    public Object Y { get; set; }
+}
+
+public class GenericPoint<Type> where Type : struct {
+    public Type X { get; set; }
+    public Type Y { get; set; }
+
+    public GenericPoint() : this(default, default) {
+
+    }
+
+    public GenericPoint(Type x, Type y) {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public void Reset() {
+        this.X = default;
+        this.Y = default;
+    }
+}
---
+++ C#/OOP/NullConcept/NullOperations/Program.cs
@@ -0,0 +1,83 @@
+﻿// Boxing: Bir kutunun içerisine bir değer koymaktır.
+Object @object = "Bir nesne";
+@object = 9;
+@object = false;
+@object = new Random();
+
+Random random = new Random();
+
+Object empty = new Object();
+
+Int32 x = default;
+Int32 y = default;
+
+Console.WriteLine(x == y);
+Console.WriteLine(x.Equals(y));
+
+Console.WriteLine(@object == empty);
+Console.WriteLine(@object.Equals(empty));
+
+//Object.Equals(x, y);
+
+// ==: Referans
+// Equals: Değer karşılaştırır
+
+
+
+
+Address address1 = new("Hatay");
+Address address2 = new("Hatay");
+Address address3 = address1;
+
+
+Console.WriteLine($"address1 == address2 sonucu: {address1 == address2}");
+Console.WriteLine($"address1.Equals(address2) sonucu: {address1.Equals(address2)}");
+Console.WriteLine("Object.ReferenceEquals: " + Object.ReferenceEquals(address1,address2));
+
+Console.WriteLine($"address1 == address3 sonucu: {address1 == address3}");
+Console.WriteLine($"address1.Equals(address3) sonucu: {address1.Equals(address3)}");
+
+// Primitive Type => Stack'de tutulur
+String deney1 = "TURKCELL";
+String deney2 = "TURKCELL";
+String deney3 = "TURKCELLx"[..^1]; 
+// Substring [..^1] dinamik olarak son karaktere kadar alır,
+// [..8] karakter seti değişse de ilk 8 karakteri alır
+Object deney4 = deney3;
+
+Console.WriteLine($"deney1 == deney2 sonucu: {deney1 == deney2}");
+Console.WriteLine($"deney1.Equals(deney2) sonucu: {deney1.Equals(deney2)}");
+
+Console.WriteLine($"deney1 {deney1.GetHashCode()} == deney3 {deney3.GetHashCode()} sonucu: {deney1 == deney3}");
+Console.WriteLine($"deney1.Equals(deney3) sonucu: {deney1.Equals(deney3)}");
+
+Console.WriteLine($"deney1 {deney1.GetHashCode()} == deney4 {deney4.GetHashCode()} sonucu: {deney1 == deney4}");
+Console.WriteLine($"deney1 == (String)deney4 sonucu: {deney1 == (String)deney4}");
+Console.WriteLine($"deney1 == deney4 as String sonucu: {deney1 == deney4 as String}");
+Console.WriteLine($"deney1.Equals(deney4) sonucu: {deney1.Equals(deney4)}");
+Console.WriteLine($"deney4.Equals(deney1) sonucu: {deney4.Equals(deney1)}");
+
+Address? address = null; // Stackde karşılığı var ama heapde oluşmamış
+Console.WriteLine(address ??= new Address("Burdur"));
+Console.WriteLine(address.City);
+
+public class Address {
+    public String City { get; set; }
+
+    public Address(String city) {
+        this.City = city;
+    }
+
+    public override Boolean Equals(Object? obj) {
+        Address? that = obj as Address;
+        return this.City.Equals(that?.City);
+    }
+
+    public override String ToString() {
+        return $"Bu adres, {this.City} şehrinde bulunuyor.";
+    }
+
+    public override Int32 GetHashCode() {
+        return base.GetHashCode();
+    }
+}
