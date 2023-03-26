fc6c4c91fa631895e8e5088e74d2f0b7e33d50dd
wiaoj <bertandeniz7@gmail.com>
2023-03-27 01:58:44 +0300

    homework 4 O - Open/Closed Principle

---
+++ C#/Homeworks/homework4/OpenClosed/Program.cs
@@ -0,0 +1,42 @@
+﻿// O - Open/Closed Principle
+
+using OpenClosed;
+
+
+SquareShape square = new(new Edge(10));
+square.Print();
+
+RectangleShape rectangle = new(new Width(10), new Height(7));
+rectangle.Print();
+
+CircleShape circle = new(new Radius(10));
+circle.Print();
+
+TriangleShape triangle = new(new Base(10), new Height(5));
+triangle.Print();
+
+// Bir kare kutu olduğunu düşünürsek, bu kutunun 6 tane karesi vardır
+SquareShape boxSurface = new(new Edge(17));
+SquareBox box = new(boxSurface);
+box.Print();
+
+/*
+ * Area of the Square: 100
+ * Area of the Rectangle: 70
+ * Area of the Circle: 314,16
+ * Area of the Triangle: 25
+ * Area of the Shape: 1734
+*/
+
+public class SquareBox : Shape {
+    public const Int32 SURFACE_COUNT = 6;
+    public SquareShape SquareShape { get; init; }
+
+    public SquareBox(SquareShape squareShape) {
+        this.SquareShape = squareShape;
+    }
+
+    public override Double Area() {
+        return this.SquareShape.Area() * SURFACE_COUNT;
+    }
+}
---
+++ C#/Homeworks/homework4/OpenClosed/Shape.cs
@@ -0,0 +1,76 @@
+﻿namespace OpenClosed;
+public abstract class Shape {
+    public abstract Double Area();
+    protected virtual Type ShapeType => typeof(Shape);
+
+    public void Print() {
+        String shapeName = this.ShapeType.Name.EndsWith(nameof(Shape), StringComparison.InvariantCultureIgnoreCase)
+            && this.ShapeType.Name is not nameof(Shape)
+            ? this.ShapeType.Name[..^nameof(Shape).Length]
+            : this.ShapeType.Name;
+
+        Console.WriteLine($"Area of the {shapeName}: {this.Area():.##}");
+    }
+}
+
+public class SquareShape : Shape {
+    public Edge Edge { get; init; }
+    protected override Type ShapeType => typeof(SquareShape);
+
+    public SquareShape(Edge edge) {
+        this.Edge = edge;
+    }
+
+    public override Double Area() {
+        return Math.Pow(this.Edge.Value, 2);
+    }
+}
+
+public class RectangleShape : Shape {
+    public Width Width { get; init; }
+    public Height Height { get; init; }
+    protected override Type ShapeType => typeof(RectangleShape);
+
+    public RectangleShape(Width width, Height height) {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public override Double Area() {
+        return this.Width.Value * this.Height.Value;
+    }
+}
+
+public class CircleShape : Shape {
+    public Radius Radius { get; init; }
+    protected override Type ShapeType => typeof(CircleShape);
+
+    public CircleShape(Radius radius) {
+        this.Radius = radius;
+    }
+
+    public override Double Area() {
+        return Math.PI * Math.Pow(this.Radius.Value, 2);
+    }
+}
+
+public class TriangleShape : Shape {
+    public Base Base { get; init; }
+    public Height Height { get; init; }
+    protected override Type ShapeType => typeof(TriangleShape);
+
+    public TriangleShape(Base @base, Height height) {
+        this.Base = @base;
+        this.Height = height;
+    }
+
+    public override Double Area() {
+        return this.Base.Value * this.Height.Value * .5;
+    }
+}
+
+public record Edge(Double Value);
+public record Width(Double Value);
+public record Height(Double Value);
+public record Radius(Double Value);
+public record Base(Double Value);
